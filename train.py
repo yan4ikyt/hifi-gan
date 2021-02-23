77571fc5df12588a08c58e24bf551faa64ed8ae0
SortAnon <56806675+SortAnon@users.noreply.github.com>
2021-02-23 13:31:57 +0100

    Update train.py

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ from env import AttrDict, build_env
 from meldataset import MelDataset, mel_spectrogram, get_dataset_filelist
 from models import Generator, MultiPeriodDiscriminator, MultiScaleDiscriminator, feature_loss, generator_loss,\
     discriminator_loss
-from utils import plot_spectrogram, scan_checkpoint, load_checkpoint, save_checkpoint
+from hifiutils import plot_spectrogram, scan_checkpoint, load_checkpoint, save_checkpoint
 
 torch.backends.cudnn.benchmark = True
 
